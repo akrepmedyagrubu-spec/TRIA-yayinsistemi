4e7e319745aee07d25761a62185acda8333f3343
akrepmedyagrubu-spec <akrepmedyagrubu@gmail.com>
2026-09-25 21:46:27 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,50 +2,49 @@
 
 mkdir -p hls
 
+
 python3 -m http.server 10000 &
 
 
-FILTER=""
-
-INDEX=0
+INPUTS=""
+COUNT=0
 
 while read line
 do
   FILE=$(echo $line | cut -d"'" -f2)
+  INPUTS="$INPUTS -re -i $FILE"
+  COUNT=$((COUNT+1))
+done < playlist.txt
+
+
+FILTER=""
+CONCAT_INPUT=""
+
+for ((i=0;i<$COUNT;i++))
+do
+  FILE=$(sed -n "$((i+1))p" playlist.txt | cut -d"'" -f2)
 
   if [[ "$FILE" == *"reklam"* ]]; then
-    FILTER+="[$INDEX:v][1:v]overlay=0:0[v$INDEX];"
+    FILTER="$FILTER [$i:v]scale=960:540[v$i]; [v$i][${COUNT}:v]overlay=0:0[v${i}o];"
   else
-    FILTER+="[$INDEX:v][2:v]overlay=0:0[v$INDEX];"
+    FILTER="$FILTER [$i:v]scale=960:540[v$i]; [v$i][$(($COUNT+1)):v]overlay=0:0[v${i}o];"
   fi
 
-  INDEX=$((INDEX+1))
-done < playlist.txt
+  CONCAT_INPUT="$CONCAT_INPUT [v${i}o][$i:a]"
+done
 
 
-INPUTS=""
-INDEX=0
-
-while read line
-do
-  FILE=$(echo $line | cut -d"'" -f2)
-  INPUTS+=" -i $FILE"
-  INDEX=$((INDEX+1))
-done < playlist.txt
-
-ffmpeg -re $INPUTS -i assets/reklam.png -i assets/yayin.png \
+ffmpeg $INPUTS -i assets/reklam.png -i assets/yayin.png \
 -filter_complex "
-$(for i in $(seq 0 $((INDEX-1))); do echo "[$i:v]scale=960:540[v$i];"; done)
 $FILTER
-$(for i in $(seq 0 $((INDEX-1))); do echo "[v$i]"; done)concat=n=$INDEX:v=1:a=0[outv]
+$CONCAT_INPUT concat=n=$COUNT:v=1:a=1[outv][outa]
 " \
--map "[outv]" \
--map 0:a \
+-map "[outv]" -map "[outa]" \
 -c:v libx264 -preset veryfast -tune zerolatency \
 -b:v 800k -maxrate 800k -bufsize 1200k \
 -c:a aac -b:a 96k \
 -f hls \
 -hls_time 2 \
--hls_list_size 5 \
--hls_flags delete_segments \
+-hls_list_size 6 \
+-hls_flags delete_segments+append_list \
 hls/stream.m3u8
