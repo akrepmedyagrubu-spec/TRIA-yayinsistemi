6577f0827654bf7934c092082b97802e99b1378e
akrepmedyagrubu-spec <akrepmedyagrubu@gmail.com>
2026-09-25 21:39:18 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,35 +2,50 @@
 
 mkdir -p hls
 
-
 python3 -m http.server 10000 &
 
-while true
+
+FILTER=""
+
+INDEX=0
+
+while read line
 do
-  while read line
-  do
-    FILE="$line"
+  FILE=$(echo $line | cut -d"'" -f2)
 
-    
-    if [[ "$FILE" == *"reklam"* ]]; then
-      LOGO="assets/reklam.png"
-    else
-      LOGO="assets/yayin.png"
-    fi
+  if [[ "$FILE" == *"reklam"* ]]; then
+    FILTER+="[$INDEX:v][1:v]overlay=0:0[v$INDEX];"
+  else
+    FILTER+="[$INDEX:v][2:v]overlay=0:0[v$INDEX];"
+  fi
 
-    echo "Oynatılıyor: $FILE | Logo: $LOGO"
+  INDEX=$((INDEX+1))
+done < playlist.txt
 
-    ffmpeg -re -i "$FILE" -i "$LOGO" \
-    -filter_complex "[1:v]scale=960:540[logo];[0:v]scale=960:540[vid];[vid][logo]overlay=0:0:format=auto" \
-    -map 0:v -map 0:a \
-    -c:v libx264 -preset veryfast -tune zerolatency \
-    -b:v 800k -maxrate 800k -bufsize 1200k \
-    -c:a aac -b:a 96k \
-    -f hls \
-    -hls_time 4 \
-    -hls_list_size 5 \
-    -hls_flags delete_segments \
-    hls/stream.m3u8
 
-  done < playlist.txt
-done
+INPUTS=""
+INDEX=0
+
+while read line
+do
+  FILE=$(echo $line | cut -d"'" -f2)
+  INPUTS+=" -i $FILE"
+  INDEX=$((INDEX+1))
+done < playlist.txt
+
+ffmpeg -re $INPUTS -i assets/reklam.png -i assets/yayin.png \
+-filter_complex "
+$(for i in $(seq 0 $((INDEX-1))); do echo "[$i:v]scale=960:540[v$i];"; done)
+$FILTER
+$(for i in $(seq 0 $((INDEX-1))); do echo "[v$i]"; done)concat=n=$INDEX:v=1:a=0[outv]
+" \
+-map "[outv]" \
+-map 0:a \
+-c:v libx264 -preset veryfast -tune zerolatency \
+-b:v 800k -maxrate 800k -bufsize 1200k \
+-c:a aac -b:a 96k \
+-f hls \
+-hls_time 2 \
+-hls_list_size 5 \
+-hls_flags delete_segments \
+hls/stream.m3u8
